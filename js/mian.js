5ae64fc98d86e77ff5cc68802e28dd787ecdc967
dariopalmesi <dariopalmesi88@gmail.com>
2024-09-30 16:28:29 +0200

    upgrade log commit

--- js/mian.js
+++ js/mian.js
@@ -6,7 +6,7 @@ let age;
 let km;
 
 // seconda face: raccolta dati
-const priceNumber = Number(window.prompt("Prezzo", ""));
+const priceNumber = Number(window.prompt("Prezzo al kilometro", ""));
 console.log(priceNumber);
 
 const ageNumber = Number(window.prompt("Età", ""));
@@ -14,3 +14,11 @@ console.log(ageNumber);
 
 const kmNumber = Number(window.prompt("Kilometri"));
 console.log(kmNumber);
+
+// terza fase: elaborazione dati
+if (ageNumber > 18 && ageNumber < 65) {
+    console.log(priceNumber * kmNumber);
+} else if (ageNumber < 18) {
+    console.log((priceNumber * kmNumber)*20 / 100 );
+    
+}
