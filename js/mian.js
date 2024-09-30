17c9ec411cd073d9c1a5cc67a07a9082b7b9922a
dariopalmesi <dariopalmesi88@gmail.com>
2024-09-30 15:50:00 +0200

    initial train commit

---
+++ js/mian.js
@@ -0,0 +1,11 @@
+console.log('ciao');
+
+// prima fase: preparazione
+let price;
+let age;
+let km;
+
+// seconda face: raccolta dati
+const priceNumber = Number(window.prompt("Type a number", ""));
+console.log(priceNumber);
+
