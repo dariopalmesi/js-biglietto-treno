de82546908665683e18e484bc9e44f4b27bafa31
dariopalmesi <dariopalmesi88@gmail.com>
2024-09-30 15:57:01 +0200

    upgrade promt commit

--- js/mian.js
+++ js/mian.js
@@ -6,6 +6,11 @@ let age;
 let km;
 
 // seconda face: raccolta dati
-const priceNumber = Number(window.prompt("Type a number", ""));
+const priceNumber = Number(window.prompt("Prezzo", ""));
 console.log(priceNumber);
 
+const ageNumber = Number(window.prompt("Età", ""));
+console.log(ageNumber);
+
+const kmNumber = Number(window.prompt("Kilometri"));
+console.log(kmNumber);
